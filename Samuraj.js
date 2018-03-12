26c9dbefe0645beb8e48cf1d3fa0c73405b54679
PatiKoszi <patrykkosznik@gmail.com>
2018-03-12 01:54:09 +0100

    const let var

--- Samuraj.js
+++ Samuraj.js
@@ -1,22 +1,36 @@
-var a = "zmienna globalna";
 
-function one() {
-    var a = "ju";
-    console.log("zmienna1 " + a);
+ function test() {
+     var z = true;
+     if (true) {
+         var z = false;
+         console.log(z);
+     }
+     console.log(z);
+ }
+ test();
 
-    function two() {
-            var a = 12;
-            console.log("zmienna2 " + a);
+ function test1() {
+     let z = true;
+     if (true) {
+         let z = false;
+         console.log(z);
+     }
+     console.log(z);
+ }
+ test1();
 
-            function tree() {
-                var a = 67;
-                console.log(`zmienna3 ${a}`);
+ function test2() {
+     let z = true;
+     if (true) {
+         let z = false;
+         console.log(z);
+     }
+     console.log(z);
+ }
+ test2();
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ //let i const nie pozwolą nadpisac, nie działa w hoistingu
+ //var pozwoli nadpisac zmienna, bedziw widoczne w objekcie globalnym window
 
-            }
-            tree();
-        }
-        two()
-}
-one();
-
-console.log("główna zmienna to: " + a);
+ //const w typie prostym zabezpiecza wartość,
+ // NIE w objekcie -tablica ten sam objekt ale może się zmieniać
